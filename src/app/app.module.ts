7ae0ccd704d80ea4c829cefb8530021de8bc931b
aqua_phoenix <aqua_phoenix@AQUANB>
2021-04-28 11:55:31 +0400

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RESTService} from './services/REST.service';
+import {HttpRepo} from './repositories/httpRepo';
+import {TodoListRepository} from './repositories/todoListRepository';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {SignButtonComponent} from './sign-button/sign-button.component';
+import {AuthService} from './services/auth.service';
+import {UserListComponent} from './main/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignButtonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+      },
+      {path: 'list', component: UserListComponent},
+      {path: '**', redirectTo: '/list'}
+    ])
   ],
-  providers: [],
+  providers: [{provide: RESTService, useValue: `http://${location.hostname}:3500/`}, HttpRepo, TodoListRepository, AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
